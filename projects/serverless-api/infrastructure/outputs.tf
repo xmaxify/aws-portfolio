8f1d62478cb785ab0b1d23668a00237d5e79e321
Maksymilian Grzenkowicz <maksymilian.grzenkowicz@atlasus.com>
2026-04-14 18:35:57 +0200

    refactor: replace hardcoded api endpoint with resource references

--- projects/serverless-api/infrastructure/outputs.tf
+++ projects/serverless-api/infrastructure/outputs.tf
@@ -1,4 +1,4 @@
 output "api_endpoint" {
   description = "Visitor counter API endpoint"
-  value       = "https://api.maxify.sh/visitors"
+  value       = "https://${aws_apigatewayv2_domain_name.api.domain_name}${regex("^[A-Z]+ (/.*)", aws_apigatewayv2_route.visit.route_key)[0]}"
 }
