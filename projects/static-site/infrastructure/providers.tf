365b040af2876f699c04892b53884705d3a6c8b7
Maksymilian Grzenkowicz <maksymilian.grzenkowicz@atlasus.com>
2026-04-14 18:00:58 +0200

    docs: add AWS documentation link to ACM

--- projects/static-site/infrastructure/providers.tf
+++ projects/static-site/infrastructure/providers.tf
@@ -15,6 +15,7 @@ provider "cloudflare" {
 }
 
 # ACM certificates for CloudFront must be created in us-east-1
+# https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/cnames-and-https-requirements.html#https-requirements-aws-region
 provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
